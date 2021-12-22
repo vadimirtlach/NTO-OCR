9827c045940cc61bc0ddfce840bb22ed34c5d662
vad13irt <w13vadirt@gmail.com>
2021-12-22 17:52:44 +0200

    initial

---
+++ dataset.py
@@ -0,0 +1,104 @@
+from torch import DataLoader
+import numpy as np
+import string 
+import cv2
+
+
+class Dataset:
+    russian_lower_letters = list("абвгдеёжзийклмнопрстуфхцчшщъыьэюя")
+    russian_upper_letters = list("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ")
+    english_lower_letters = list(string.ascii_lowercase)
+    english_upper_letters = list(string.ascii_uppercase)
+    digits = list(string.digits)
+    puntuations = list(string.punctuation + " ")
+    special_tokens = ["<BOS>", "<PAD>", "<EOS>", "<OOV>"]
+    characters = special_tokens + russian_lower_letters + russian_upper_letters + english_lower_letters + english_upper_letters + digits + puntuations
+    classes = {idx: character for idx, character in enumerate(characters)}
+    reversed_classes = {character: idx for idx, character in enumerate(characters)}
+    
+    oov_token_index = reversed_classes["<OOV>"]
+    bos_token_index = reversed_classes["<BOS>"]
+    pad_token_index = reversed_classes["<PAD>"]
+    eos_token_index = reversed_classes["<EOS>"]
+    
+    def __init__(self, pathes, labels=None, transforms=None, max_length=25):
+        self.pathes = pathes
+        self.labels = labels
+        self.transforms = transforms
+        self.max_length = max_length
+        
+    def __len__(self):
+        return len(self.pathes)
+    
+    @staticmethod
+    def text2label(text):
+        label = []
+        for _ in list(text):
+            encoded = Dataset.reversed_classes.get(str(_), Dataset.oov_token_index)
+            label.append(encoded)
+            
+        label = np.array(label)
+        return label
+    
+    @staticmethod
+    def label2text(label):
+        text = []
+        for _ in label:
+            decoded = Dataset.classes.get(int(_), Dataset.classes[Dataset.oov_token_index])
+    
+            if decoded not in Dataset.special_tokens:
+                text.append(decoded)
+        
+        text = "".join(text)
+        return text
+    
+    def add_special_tokens(self, label):
+        label = list(label)
+        num_paddings = self.max_length - len(label)
+        label = [Dataset.bos_token_index] + label + [Dataset.pad_token_index] * num_paddings  + [Dataset.eos_token_index]
+        label = np.array(label)
+        return label
+    
+    def __getitem__(self, index):
+        path = self.pathes[index]
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = np.expand_dims(image,axis=-1)
+        
+        if self.transforms:
+            image = self.transforms(image=image)["image"]
+        
+        if self.labels is not None:
+            label = self.labels[index]
+            label = list(Dataset.text2label(label))
+            label = self.add_special_tokens(label)
+            return image, label
+        
+        return image
+    
+    @staticmethod
+    def create_loader(pathes, labels=None, transforms=None, max_length=25, batch_size=16, pin_memory=False, num_workers=0, shuffle=False, drop_last=False):
+        dataset = Dataset(pathes=pathes, 
+                          labels=labels, 
+                          max_length=max_length,
+                          transforms=transforms)
+        
+        loader = DataLoader(dataset=dataset, 
+                            batch_size=batch_size, 
+                            pin_memory=pin_memory, 
+                            num_workers=num_workers, 
+                            shuffle=shuffle, 
+                            drop_last=drop_last)
+        
+        return dataset, loader
+    
+    @staticmethod
+    def collate_batch(batch, inputs_device="cpu", labels_device="cpu"):
+        if len(batch) > 1:
+            inputs, labels = batch
+            inputs = inputs.to(inputs_device).float()
+            labels = labels.to(labels_device).float()
+            
+            return inputs, labels
+        else:
+            return batch.to(inputs_device)
