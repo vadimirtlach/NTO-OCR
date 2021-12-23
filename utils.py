5066469946b8b260e86c9e9a986dc2d1f0addb4a
vad13irt <w13vadirt@gmail.com>
2021-12-23 17:55:24 +0200

    added utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+import random
+import os
+import torch
+from torch import nn
+import numpy as np
+
+
+def seed_everything(seed:int=42) -> None:
+    random.seed(seed)
+    os.environ['PYTHONASSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
